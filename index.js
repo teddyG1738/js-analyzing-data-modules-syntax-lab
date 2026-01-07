79e756a3460ef8fae4288f17e08957fd658796da
teddyG1738 <ted.githaiga@student.moringaschool.com>
2026-01-07 21:35:16 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+function combineUsers(...arrays) {
+  const merged = arrays.flat();
+  const users = Array.from(new Set(merged)); // preserve first occurrence, remove duplicates
+  const now = new Date();
+  const merge_date = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
+  return { users, merge_date };
+}
 
-
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = { combineUsers };
